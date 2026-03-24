07cde3b56c57b95ffd3d90212ea6d42bc569eb48
shooukie <bigcape@naver.com>
2026-03-24 18:30:07 +0900

    refactor: 충돌 2차 해결

--- src/components/path.ts
+++ src/components/path.ts
@@ -3,12 +3,13 @@ export const PATH = {
   // 1. Public (로그인 전 접속 가능)
   ONBOARDING: "/",
   LOGIN: "/login",
+  SIGNUP: "/signUp",
 
   // 2. Protected (로그인이 필요한 메인 메뉴)
   HOME: "/home",
   BOARD: "/board",
   PROJECTS: "/project", // 파일 이름인 projects.tsx와 주소 일치
-  
+
   // 3. MyPage (pages/my/ 서브 폴더 구조 반영)
   MY: "/my",
   MY_POSTS: "/my/posts", // pages/my/myPost.tsx 로 라우팅
